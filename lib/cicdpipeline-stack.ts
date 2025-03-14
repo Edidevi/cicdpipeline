0a84791897b9dfa7648d56f0dde9fa97c5f3dd65
Edidevi <cloudshell-user@ip-10-134-45-13.ec2.internal>
2025-03-14 12:37:45 +0000

    lib file edit

--- lib/cicdpipeline-stack.ts
+++ lib/cicdpipeline-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep  } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './stage';
+// import { MyPipelineAppStage } from './stage';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -10,7 +10,7 @@ export class CicdpipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
   
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    new CodePipeline(this, 'Pipeline', {
       pipelineName: 'OsaruPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('Edidevi/cicdpipeline', 'main'),
