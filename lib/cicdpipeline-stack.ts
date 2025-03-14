030144d70e5cf9bf23ce4a6146c6b9bf635ef5d6
Edidevi <cloudshell-user@ip-10-134-45-13.ec2.internal>
2025-03-14 15:34:33 +0000

    second edits

--- lib/cicdpipeline-stack.ts
+++ lib/cicdpipeline-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 import { CodePipeline, CodePipelineSource, ShellStep  } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-// import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -10,7 +10,7 @@ export class CicdpipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
   
-    new CodePipeline(this, 'Pipeline', {
+   const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'OsaruPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('Edidevi/cicdpipeline', 'main'),
@@ -19,6 +19,20 @@ export class CicdpipelineStack extends cdk.Stack {
                     'npx cdk synth']
       })
     });
+
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "701182342014", region: "us-east-1" }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "701182342014", region: "us-east-1" }
+    }));
+
+
+
   }
 }
 
