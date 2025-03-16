17569acea822ae6d8737e3a1dac281acd22251f4
Edidevi <cloudshell-user@ip-10-138-163-167.ec2.internal>
2025-03-16 16:47:51 +0000

    5th debug

--- lib/lambda/handler.ts
+++
@@ -1,7 +0,0 @@
-export async function handler(event: string, context: string) {
-  console.log('Stage Name is: ' + process.env.stage);
-  return {
-    body: 'Hello from a Lambda function',
-    statusCode: 200,
-  };
-}
