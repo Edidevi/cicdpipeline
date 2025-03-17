5eefa778fe85264ee6229b281ed1b98ea9c6e58e
Edidevi <cloudshell-user@ip-10-130-72-202.ec2.internal>
2025-03-17 15:22:49 +0000

    we don't quit

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -8,7 +8,7 @@ export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
       super(scope, id, props);
       new Function(this, 'LambdaFunction', {
-        runtime: Runtime.NODEJS_12_X,
+        runtime: Runtime.NODEJS_22_X,
         handler: 'handler.handler',
         code: Code.fromAsset(path.join(__dirname, 'lambda')),
         environment: { "stageName": stageName }
